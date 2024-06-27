355a34482e9200917770fa010e3b7c348d7bdac7
Dev Govindji <dev.govindji.software@gmail.com>
2024-06-27 16:33:35 -0500

    Finishes Mobile Detections implementation

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,9 @@
 // src/LandingPage.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import {isMobile} from 'react-device-detect';
+// import {BrowserView, MobileView} from 'react-device-detect';
+
 
 const LandingPage = () => {
     
@@ -10,14 +13,46 @@ const LandingPage = () => {
         navigate('/demo');
     };
 
-    return (
-        <div className="landing-container" onClick={handleEnterClick}>
-            <h1 className="enter-title">Welcome to the Govindji's VIP Experience</h1>
-                <button className="enter-button" onClick={handleEnterClick}>
-                    Enter
-                </button>
-        </div>
-    );
+    // renderContent = () => {
+        if (isMobile) {
+          return (
+            <>
+                <h1 className="mobile-message-title">Mobile Experience Coming Soon</h1>
+                <p className="mobile-message-message">Please view on your fullscreen desktop for an optimal experience.</p>
+            </>
+          )
+        }
+        return (
+            <>
+                <div className="landing-container" onClick={handleEnterClick}>
+                    <h1 className="enter-title">Welcome to the Govindji's VIP Experience</h1>
+                        <button className="enter-button" onClick={handleEnterClick}>
+                            Enter
+                        </button>
+                </div>
+            </>
+        )
+    //   }
+
+    // return (
+    //     <>
+
+        
+    //         <MobileView className="mobile-message-container">
+    //             <h1 className="mobile-message-title">Mobile Experience Coming Soon</h1>
+    //             <p className="mobile-message-message">Please view on your fullscreen desktop for an optimal experience.</p>
+    //         </MobileView>
+
+    //         <BrowserView>
+    //             <div className="landing-container" onClick={handleEnterClick}>
+    //                 <h1 className="enter-title">Welcome to the Govindji's VIP Experience</h1>
+    //                     <button className="enter-button" onClick={handleEnterClick}>
+    //                         Enter
+    //                     </button>
+    //             </div>
+    //         </BrowserView>
+    //     </>
+    // );
 };
 
 export default LandingPage;
